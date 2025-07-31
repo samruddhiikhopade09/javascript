c1b3133e4248337ec82b265c8fff827634372a51
samruddhiikhopade09 <samruddhikhopade09@gmail.com>
2025-07-31 22:19:59 +0530

    pop up box

---
+++ Day 3/script.js
@@ -0,0 +1,19 @@
+// pop up box
+
+
+// 1 alert
+
+//alert("helloo")
+
+//ert("are you ok")
+
+
+// 2 prompt box
+
+//let name = prompt("enter your name")
+//console.log(name)
+
+let first = number (prompt("enter first number"))
+let second = number (prompt("enter second number"))
+
+console.log(first + second)

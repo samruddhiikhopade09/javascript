3d15ef9cc142563b14f6cc7017e1f41f32886cba
samruddhiikhopade09 <samruddhikhopade09@gmail.com>
2025-07-31 23:29:52 +0530

    operators in javascript

--- Day 3/script.js
+++ Day 3/script.js
@@ -3,17 +3,22 @@
 
 // 1 alert
 
-//alert("helloo")
+alert("helloo")
 
-//ert("are you ok")
+alert("are you ok")
 
 
 // 2 prompt box
 
-//let name = prompt("enter your name")
-//console.log(name)
+let name = prompt("enter your name")
+console.log(name)
 
 let first = number (prompt("enter first number"))
 let second = number (prompt("enter second number"))
 
 console.log(first + second)
+
+
+// confirm
+
+confirm("are you sure")
---
+++ Day 4/script.js
@@ -0,0 +1,12 @@
+// operators in javascript 
+
+
+//arithemetic operator
+
+let first_no = 100
+let sec_no = 500
+
+//console.log(first_no + sec_no)
+
+console.log(first_no - sec_no)
+

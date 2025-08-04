928ae504292d93bbc3310bb09e868816d7ff9f97
samruddhiikhopade09 <samruddhikhopade09@gmail.com>
2025-08-04 22:54:52 +0530

    day 5 of javascript

--- Day 4/script.js
+++ Day 4/script.js
@@ -3,10 +3,81 @@
 
 //arithemetic operator
 
-let first_no = 100
-let sec_no = 500
+//let first_no = 100
+//let sec_no = 500
 
 //console.log(first_no + sec_no)
 
-console.log(first_no - sec_no)
+//console.log(first_no - sec_no)
+
+//let a = 300;
+//let b = 900;
+
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+//console.log(a % b)
+//console.log(a ** b)
+
+//assignments operators
+let xyz =100
+console.log(xyz)
+
+let pqr = 10000
+console.log(pqr)
+
+// add nd assignment
+
+let a += 10;
+console.log(a)
+
+let b -= 10;
+console.log(b)
+
+let c *= 10;
+console.log(c)
+
+let d /= 10;
+console.log(d)
+
+e %= 10;
+console.log(e)
+
+
+
+
+// comparison operator
+
+let x = 40
+let y = 50
+
+console.log(x == 40)
+console.log(y == 50)
+
+
+// logical operator 
+
+//1. logical AND(&&)
+// 2. logical  OR(||)
+//3.  logucal NOT(!)
+
+let  a= 20;
+let b= 20;
+
+console.log(a>6 && b<25)
+console.log(a>=20 || b<=25)
+
+console.log(! true)
+console.log(! false)
+
+
+
+
+
+
+
+
+
+
 
---
+++ Day 5/script.js
@@ -0,0 +1,50 @@
+// condition statement
+
+if ( 10 == 10){
+    console.log(true)
+}
+
+let user == "admin"
+
+if(user == "admin){
+
+    console.log("welcome user")
+}
+
+
+// even no.s
+
+const number = 5;
+
+if(number % 2 == 0){
+    console.log(number is even)
+}
+
+// even or odd no.s
+
+let number = 2;
+if(number % 2 == 0){
+    console.log(number is even)
+
+}else{
+    console.log("number is odd")
+}
+
+// prompt
+
+let numbers = prompt("enter the number")
+
+if(number % 2 == 0){
+    console.log("no is even")
+}else{
+    console.log("no is odd")
+}
+
+// token
+
+const token ="abcdefghi0309"
+if (token){
+    console.log("login")
+}else{
+    console.log(cant find page)
+}

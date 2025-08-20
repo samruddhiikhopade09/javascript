e7325ee20cb619dda33584a7b15ad30efe76dbbe
samruddhiikhopade09 <samruddhikhopade09@gmail.com>
2025-08-20 22:21:19 +0530

    for loop day 10

---
+++ Day 10/script.js
@@ -0,0 +1,35 @@
+// let colors =['red','pink','white']
+
+// let fruits -['guava','watermelom,'mango']
+
+// let day =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+
+//  even no.s from 1-10
+
+// let Even_no=[2,4,6,8]
+
+
+//let animal_name=['cat','cow','ox','rat','mouse']
+
+// let colors =['red','pink','white', 'gray','brown','purple','lavender']
+
+// for (let i = 0; i< colors.length; i++)
+//{
+  // console.log(colors[i])
+//}
+
+// let cities =['pune','mumbai'.'satara','nagpur','solapur']
+//for(i=0; i< cities.length; i++)
+// {
+   //console.log(cities)
+//} 
+
+// let courses=['it','java','pythin','javascrpt']
+//for(i=0; i< courses.length; i++)
+//{
+  //  console.log(courses)
+//}
+
+
+ let result= courses.pop()
+console.log(result)
